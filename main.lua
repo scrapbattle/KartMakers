cbd42f1dd87fcdf05855767c44015b7fe4ed10df
RainlessSky <174295816+RainlessSky@users.noreply.github.com>
2025-07-05 15:34:03 -0700

    Add banned block system, which disables engines if a banned block is present. Added engine limiter, disabling engines if more than one engine is present.

--- main.lua
+++ main.lua
@@ -15,7 +15,10 @@ function OnPlayerJoined(player)
     player_data[player.playerId] = {
         total_buoyancy = 0,
         total_weight = 0,
-        block_count = 0
+        block_count = 0,
+        has_banned_blocks = false,
+        banned_blocks = {},
+        total_engines = 0
     }
 end
 tm.players.OnPlayerJoined.add(OnPlayerJoined)
@@ -36,22 +39,31 @@ local block_types = {
     WaterSki = "4x1x1",
     Piston = "4x1x1",
 
-    -- Hinge having no drag is debatable because it honestly doesnt matter that much
+    -- Hinge having less drag is debatable because it honestly doesnt matter that much
     SmallHinge = "decoration",
 
-    -- pipes suck for aerodynamics as decoration pieces but if rainless is reading this,
-    -- the intention is for pipes to have the same drag as 2x1x1 wedges
-    -- however i have no clue how much drag it has so for now it will be zero drag
-    ModularTubeSystem_Tube1 = "decoration",
-    ModularTubeSystem_Skewe = "decoration",
-    ModularTubeSystem_TubeE = "decoration",
-    ModularTubeSystem_Tube3 = "decoration",
+    ModularTubeSystem_TubeDoubleElbow = "decoration",
+    ModularTubeSystem_Skewed1x1x2 = "decoration",
+    ModularTubeSystem_Tube1x1x8 = "decoration",
+    ModularTubeSystem_Skewed2x1x2 = "decoration",
+    ModularTubeSystem_TubeTeeCross = "decoration",
+    ModularTubeSystem_DoubleCross = "decoration",
+    ModularTubeSystem_Tube3x1 = "decoration",
+    ModularTubeSystem_TubeDoubleTee = "decoration",
+    ModularTubeSystem_Tube1x4 = "decoration",
+    ModularTubeSystem_TubeTee = "decoration",
+    ModularTubeSystem_Tube1x1 = "decoration",
+    ModularTubeSystem_TubeElbow = "decoration",
+    ModularTubeSystem_TubeCross = "decoration",
     LightFront = "decoration",
+    LightFrontLarge = "decoration",
     LightFront_V2 = "decoration",
+    LightBack = "decoration",
+    LightBack_V2 = "decoration",
     PopupHeadlightBlock = "decoration",
-    MilitaryHeadlights = "decoration",
-    CityHeadlights = "decoration",
     BajaLEDs = "decoration",
+    CityHeadlights = "decoration",
+    MilitaryHeadlights = "decoration",
     CameraBlock = "decoration",
 
     -- no drag for the seats
@@ -63,18 +75,73 @@ local block_types = {
     -- if you have any ideas for other pieces to reduce or no drag feel free to put them here if they are decorative parts on your kart
     -- note to self: find a way to boost performance of bikes because they are severely underpowered
 
-    EngineNinja = "engine"
+    EngineBasic = "engine",
+    EngineOlSchool = "engine",
+    EngineNinja = "engine",
+
+    -- Banned blocks
+    -- Banned blocks: Traction
+    AnchorBlock = "banned",
+    HoveringBlock = "banned",
+    -- Banned blocks: Mechanical
+    DetachableBlock = "banned",
+    PowerConduitConnector = "banned",
+    -- Banned blocks: Air propellers
+    PropellerBlock = "banned",
+    LargePropeller = "banned",
+    HelicopterTailPropeller = "banned",
+    HugePropellerEngine = "banned",
+    -- Banned blocks: Water propellers
+    UnderWaterPropeller = "banned",
+    BulldawgBoatEngineBlock = "banned",
+    -- Banned blocks: Thrusters
+    JetEngineLarge = "banned",
+    JetGimbal = "banned",
+    JetEngine = "banned",
+    -- Banned blocks: Lift
+    ModularWingSmall = "banned",
+    WingFlapSmall = "banned",
+    ModularWing = "banned",
+    AirBalloon = "banned",
+    WingFlap = "banned",
+    SmallBalloon = "banned",
+    HeliRotator = "banned",
+    HeliRotatorv2 = "banned",
+    Helicopterblade = "banned",
+    HelicopterBladeShort = "banned",
+    HelicopterbladeLong = "banned",
+    -- Banned blocks: Weapons
+    AimingServo = "banned",
+    EnergyShield = "banned",
+    SmallEnergyShield = "banned",
+    BombRack = "banned",
+    Dynamite = "banned",
+    Dispenser_LandMine = "banned",
+    CannonTiny = "banned",
+    CannonSmall = "banned",
+    ShortRangeSpray = "banned",
+    Shotgun = "banned",
+    MiniGun = "banned",
+    MiniGrenadeLauncher = "banned",
+    Cannon = "banned",
+    FlakCannon_Large = "banned",
+    FlakCannon_Small = "banned",
+    TankCannon = "banned",
+    RocketLauncher = "banned",
+    GunpowderCannon = "banned",
+    TankCannonSmall = "banned",
+    SpaceBlasterTiny = "banned",
+    SpaceBlaster = "banned",
+    EMPBlaster = "banned",
+    ShortRangeEMPBlaster = "banned",
+    Flamethrower = "banned",
+    Underwater_ProjectileWeapon = "banned",
+    Underwater_ExplosiveWeapon = "banned",
+    Dispenser_DepthCharge = "banned",
+    Dispenser_Torpedos = "banned",
+    Dispenser_SeaMine = "banned"
 }
 
-local special_properties = { -- this is for displaying special properties on ui. labels CANNOT be >64 bytes because the server WILL CRASH
-    {key="3x3x1", label="<color=#FC4>+45kg Buoyancy</color>"},
-    {key="3x3x2", label="<color=#FC4>+45kg Buoyancy</color>"},
-    {key="4x1x1", label="<color=#FC4>+160kg Buoyancy, 0 drag</color>"},
-    {key="decoration", label="<color=#FC4>0 drag</color>"},
-    {key="seat", label="<color=#FC4>0 drag</color>"},
-
-    {key="engine", label="<color=#FC4>Read UI for Power statistic</color>"},
-}
 local engine_cc_list = { -- this holds the RGB color for paints, and then what engine cc they corrospond to. cc is multiplied by 22.22 when setting engine power. increase cc by multiples of 50 for clean engine power values
     {color="RGBA(1.000, 0.341, 0.133, 1.000)", cc=50},
     {color="RGBA(0.902, 0.000, 0.000, 1.000)", cc=100},
@@ -108,51 +175,77 @@ end
 function CheckStructures(playerId)
     local profiling_structure_checking_start_time = tm.os.GetRealtimeSinceStartup()
     if tm.players.GetPlayerIsInBuildMode(playerId) then
-        local live_block_count = -1
+        local live_block_count = 0
         local structures = tm.players.GetPlayerStructuresInBuild(playerId)
         for _, structure in ipairs(structures) do
             live_block_count = live_block_count + #structure.GetBlocks()
         end
         if live_block_count ~= player_data[playerId].block_count then
+            if profiling==true then
+                tm.os.Log("Build was updated for ".. tm.players.GetPlayerName(playerId).. " (".. playerId.. "), updating data...")
+            end
             player_data[playerId].total_buoyancy = 0
             player_data[playerId].total_weight = 0
-            for _, structure in ipairs(structures) do
+            player_data[playerId].has_banned_blocks = false
+            player_data[playerId].banned_blocks = {}
+            player_data[playerId].total_engines = 0
+            for _,structure in ipairs(structures) do
                 local blocks = structure.GetBlocks()
-                if profiling==true then
-                    tm.os.Log("Build has been updated for ".. tm.players.GetPlayerName(playerId).. " (".. playerId.. "), updating data...")
-                end
-                for _, block in ipairs(blocks) do
-                    if block.Exists() == true then
-                        -- multiply mass by 5 to convert to kg
-                        player_data[playerId].total_buoyancy = player_data[playerId].total_buoyancy + block.GetBuoyancy()*5
-                        player_data[playerId].total_weight = player_data[playerId].total_weight + block.GetMass()*5
+                for index,block in pairs(blocks) do
+                    local value = block_types[string.sub(block.GetName(), 5, -10)]
 
-                        local value = block_types[string.sub(block.GetName(), 5, -10)]
-                        if value == "3x3x1" or value == "3x3x2" then -- set buoyancy for common wheels
-                            block.SetBuoyancy(9) -- multiply buoyancy by 5 to convert to kg
+                    if value == "banned" then
+                        for _,block in ipairs(blocks) do
+                            local value = block_types[string.sub(block.GetName(), 5, -10)]
+                            if value == "engine" then
+                                block.SetEnginePower(0)
+                            end
                         end
-                        if value == "4x1x1" then -- skis and pistons
-                            block.SetBuoyancy(32)
-                            block.SetDragAll(0, 0, 0, 0, 0, 0) -- 0 drag pistons and skis
-                        end
-                        if value == "seat" then
-                            block.SetDragAll(0, 0, 0, 0, 0, 0) -- 0 drag 
-                        end
-                        if value == "decoration" then -- pipes, lights, steering hinge, camera block
-                            block.SetDragAll(0, 0, 0, 0, 0, 0) -- 0 drag for now, in the future make it the same as 2x1x1 wedge
-                        end
-                        -- Log the current engine's secondary color. Un comment this when adding new cc colors
-                        --if value == "engine" then tm.os.Log(block.GetSecondaryColor().ToString()) end
-                        if value == "engine" then
-                            block.SetEnginePower(0) -- Set to zero so if it's an invalid color it remains as zero
+                        player_data[playerId].has_banned_blocks = true
+                        table.insert(player_data[playerId].banned_blocks, string.sub(block.GetName(), 5, -10))
+                    end
+
+                    if value == "3x3x1" or value == "3x3x2" then -- set buoyancy for common wheels
+                        block.SetBuoyancy(9) -- multiply buoyancy by 5 to convert to kg
+                    end
+                    if value == "4x1x1" then -- skis and pistons
+                        block.SetBuoyancy(32)
+                        block.SetDragAll(0.1, 0.1, 0.1, 0.1, 0.1, 0.1) -- 0 drag pistons and skis
+                    end
+                    if value == "seat" then
+                        block.SetDragAll(0.1, 0.1, 0.1, 0.1, 0.1, 0.1) -- 0 drag 
+                    end
+                    if value == "decoration" then -- pipes, lights, steering hinge, camera block
+                        block.SetDragAll(0.1, 0.1, 0.1, 0.1, 0.1, 0.1) -- 0 drag for now, in the future make it the same as 2x1x1 wedge
+                    end
+                    -- Log the current engine's secondary color. Un comment this when adding new cc colors
+                    --if value == "engine" then tm.os.Log(block.GetSecondaryColor().ToString()) end
+                    if value == "engine" and #player_data[playerId].banned_blocks==0 then
+                        block.SetEnginePower(0) -- Set to zero so if it's an invalid color it remains as zero
+                        --tm.os.Log("Engine power is now 0")
+                        if player_data[playerId].total_engines==0 then
                             for i,_ in pairs(engine_cc_list) do
                                 if block.GetSecondaryColor().ToString()==engine_cc_list[i].color then
                                     block.SetEnginePower(engine_cc_list[i].cc*22.22)
-                                    break 
+                                    --tm.os.Log("Engine power is now ".. block.GetEnginePower())
+                                    player_data[playerId].total_engines = player_data[playerId].total_engines + 1
+                                end
+                            end
+                        else
+                            player_data[playerId].total_engines = player_data[playerId].total_engines + 1
+                            for _,block in ipairs(blocks) do
+                                local value = block_types[string.sub(block.GetName(), 5, -10)]
+                                if value == "engine" then
+                                    block.SetEnginePower(0)
+                                    --tm.os.Log("Engine power is now ".. block.GetEnginePower())
                                 end
                             end
                         end
                     end
+
+                    -- multiply mass by 5 to convert to kg
+                    player_data[playerId].total_buoyancy = player_data[playerId].total_buoyancy + block.GetBuoyancy()*5
+                    player_data[playerId].total_weight = player_data[playerId].total_weight + block.GetMass()*5
                 end
             player_data[playerId].block_count = #blocks
             end
@@ -164,10 +257,12 @@ function CheckStructures(playerId)
 end
 
 function UpdateUI(playerId)
+    local profiling_ui_start_time = tm.os.GetRealtimeSinceStartup()
     if tm.players.GetPlayerIsInBuildMode(playerId)==true then
-        local profiling_ui_start_time = tm.os.GetRealtimeSinceStartup()
+        if player_data[playerId].has_banned_blocks==false then
+        if player_data[playerId].total_engines<2 then
         tm.playerUI.AddUILabel(playerId, "totalbuoyancy", string.format("%.1f", player_data[playerId].total_buoyancy).. "kg total vehicle buoyancy")
-        tm.playerUI.AddUILabel(playerId, "totalweight", string.format("%.1f", player_data[playerId].total_weight).. "kg total vehicle weight") -- Inaccurate vehicle weight isn't a bug; steering hinge has a misleading in-game weight value
+        tm.playerUI.AddUILabel(playerId, "totalweight", string.format("%.1f", player_data[playerId].total_weight).. "kg total vehicle weight") -- Inaccurate vehicle weight isn't a bug; steering hinge has a misleading in-game weight value   
 
         if tm.players.GetPlayerSelectBlockInBuild(playerId)~=nil then
             tm.playerUI.AddUILabel(playerId, "spacer", "")
@@ -203,6 +298,15 @@ function UpdateUI(playerId)
                 else
                     tm.playerUI.AddUILabel(playerId, "selectedblock-enginepower", "<color=#FAA>Invalid secondary color!</color>") 
                 end
+            end
+        end
+        else
+            tm.playerUI.AddUILabel(playerId, "toomanyengines", "<b><color=#E22>You can only have one engine!</color></b>")
+        end
+        else
+            tm.playerUI.AddUILabel(playerId, "banned-blocks", "<b><color=#E22>Your kart has banned blocks!</color></b>")
+            for i,_ in ipairs(player_data[playerId].banned_blocks) do
+                tm.playerUI.AddUILabel(playerId, "banned-blocks-"..i, "<i>".. player_data[playerId].banned_blocks[i].. "</i>")
             end
         end
         if profiling==true then
